ba12c50dffeb605b6e64e8077758b29fc0579e80
Blake Scala <blakescala@gmail.com>
2016-09-29 23:12:01 -0700

    add animal instantiation test

---
+++ src/test/java/AnimalTest.java
@@ -0,0 +1,17 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.util.Arrays;
+import java.util.*;
+
+public class AnimalTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void animal_instantiatesCorrectly_true() {
+    Animal animal = new Animal("Brown Bear");
+    assertTrue(animal instanceof Animal);
+  }
+}
